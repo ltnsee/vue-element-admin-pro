7407a0178ccac5d04cf3fd7f29d30b9122ee7f76
ltnsee <ltnsee8@163.com>
2020-06-12 15:17:17 +0800

    first commit

---
+++ .prettierrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 200,
+  tabWidth: 2,
+  tabs: false,
+  semi: true,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  tailingComma: 'none',
+  bracketSpacing: true,
+  jsxBracketSameLine: true,
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  htmlWhitespaceSensitivity: 'ignore'
+};
