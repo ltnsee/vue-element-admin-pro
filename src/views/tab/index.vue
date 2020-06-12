9df078d97e7623e6a2ea476fdf6c65673663347b
ltnsee <ltnsee8@163.com>
2020-06-12 18:54:57 +0800

    update

--- src/views/tab/index.vue
+++ src/views/tab/index.vue
@@ -5,7 +5,7 @@
     <el-tabs v-model="activeName" style="margin-top:15px;" type="border-card">
       <el-tab-pane v-for="item in tabMapOptions" :key="item.key" :label="item.label" :name="item.key">
         <keep-alive>
-          <tab-pane v-if="activeName==item.key" :type="item.key" @create="showCreatedTimes" />
+          <tab-pane v-if="activeName == item.key" :type="item.key" @create="showCreatedTimes" />
         </keep-alive>
       </el-tab-pane>
     </el-tabs>
@@ -13,7 +13,7 @@
 </template>
 
 <script>
-import tabPane from './components/TabPane'
+import tabPane from './components/TabPane';
 
 export default {
   name: 'Tab',
@@ -28,30 +28,30 @@ export default {
       ],
       activeName: 'CN',
       createdTimes: 0
-    }
+    };
   },
   watch: {
     activeName(val) {
-      this.$router.push(`${this.$route.path}?tab=${val}`)
+      this.$router.push(`${this.$route.path}?tab=${val}`);
     }
   },
   created() {
     // init the default selected tab
-    const tab = this.$route.query.tab
+    const tab = this.$route.query.tab;
     if (tab) {
-      this.activeName = tab
+      this.activeName = tab;
     }
   },
   methods: {
     showCreatedTimes() {
-      this.createdTimes = this.createdTimes + 1
+      this.createdTimes = this.createdTimes + 1;
     }
   }
-}
+};
 </script>
 
 <style scoped>
-  .tab-container {
-    margin: 30px;
-  }
+.tab-container {
+  margin: 30px;
+}
 </style>
